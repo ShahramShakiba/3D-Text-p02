668aab12225825008db7775d5ec0f1a8668a0bac
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-06-10 12:23:40 +0330

    Put the Text in the Center

--- src/script.js
+++ src/script.js
@@ -1,20 +1,18 @@
 import * as THREE from 'three';
-import GUI from 'lil-gui';
 import { FontLoader } from 'three/examples/jsm/Addons.js';
 import { TextGeometry } from 'three/examples/jsm/Addons.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const canvas = document.querySelector('canvas.webgl'); // Canvas
 const scene = new THREE.Scene(); // Scene
-const gui = new GUI(); // Debug
 
 const axesHelper = new THREE.AxesHelper(); // Axes Helper
 // scene.add(axesHelper);
 
 //======================= Textures ========================
 const textureLoader = new THREE.TextureLoader();
-const matcapTextTexture = textureLoader.load('/textures/matcaps/1.png');
-const matcapDonutTexture = textureLoader.load('/textures/matcaps/4.png');
+const textTexture = textureLoader.load('/textures/matcaps/4.png');
+const donutTexture = textureLoader.load('/textures/matcaps/8.png');
 
 // Arrays to hold the donuts and their rotation speeds
 const donuts = [];
@@ -23,37 +21,36 @@ const rotationSpeeds = [];
 //======================= Fonts ========================
 const fontLoader = new FontLoader();
 fontLoader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
-  const textGeometry = new TextGeometry('Creative Developer \n& Curious Mind', {
-    font,
-    size: 0.4,
-    height: 0.1,
-    curveSegments: 5,
-    bevelEnabled: true,
-    bevelThickness: 0.03,
-    bevelSize: 0.02,
-    bevelOffset: 0,
-    bevelSegments: 4,
-  });
-
-  /* Center the Text */
-  /* textGeometry.computeBoundingBox();
-  textGeometry.translate(
-    -(textGeometry.boundingBox.max.x - 0.02) * 0.5, // - bevelSize
-    -(textGeometry.boundingBox.max.y - 0.02) * 0.5, // - bevelSize
-    -(textGeometry.boundingBox.max.z - 0.03) * 0.5 // - bevelThickness
-  ); */
-  textGeometry.center();
-
   const textMaterial = new THREE.MeshMatcapMaterial({
-    matcap: matcapTextTexture,
+    matcap: textTexture,
   });
-  const text = new THREE.Mesh(textGeometry, textMaterial);
-  scene.add(text);
+
+  const lines = ['Creative Developer', '&', 'Curious Mind'];
+  const lineHeight = 0.5; // Adjust the line height as needed
+
+  lines.forEach((line, index) => {
+    const textGeometry = new TextGeometry(line, {
+      font,
+      size: 0.4,
+      height: 0.1,
+      curveSegments: 5,
+      bevelEnabled: true,
+      bevelThickness: 0.03,
+      bevelSize: 0.02,
+      bevelOffset: 0,
+      bevelSegments: 4,
+    });
+    textGeometry.center();
+
+    const text = new THREE.Mesh(textGeometry, textMaterial);
+    text.position.y = index * -lineHeight;
+    scene.add(text);
+  });
 
   console.time('donuts');
 
   const donutMaterial = new THREE.MeshMatcapMaterial({
-    matcap: matcapDonutTexture,
+    matcap: donutTexture,
   });
   const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
 
@@ -124,10 +121,7 @@ window.addEventListener('resize', () => {
 });
 
 //==================== Animate ==========================
-const clock = new THREE.Clock();
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-
   donuts.forEach((donut, index) => {
     const speed = rotationSpeeds[index];
 
